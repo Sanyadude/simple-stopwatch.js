6fc66ed69a443ad9700d0ff684e4f2da2638ee3e
Sanyadude <sanyadude@i.ua>
2019-03-24 02:07:54 +0200

    add all

--- simple-timer.js
+++ simple-timer.js
@@ -1,11 +1,50 @@
-function countTime() {
-    let timeToCount = new Date().getTime(); //start timestamp
-    let startTime = new Date().getTime();
-    let interval = 100;
-    setInterval(()=>{
-        let updateDate = new Date().getTime();
-        let realTimeUpdate = updateDate - startTime;
-        let realTime = timeToCount + realTimeUpdate;
-        console.log(realTime);
-    }, interval);
+'use strict'
+
+var SimpleTimer = function(customOptions) {
+    let options = customOptions ? customOptions : {};
+    let jsSecond = 1000;
+    let time = options.startTime ? options.startTime : 0;
+    let updateInterval = options.interval ? options.interval : jsSecond;
+    let precision = options.precision ? options.precision : 0;
+    let container = document.getElementById(options.containerId);
+    let handler;
+    let formatTime = (milliseconds) =>(milliseconds/jsSecond).toFixed(precision);
+    let toTimestamp = (time) => Number(time)*jsSecond;
+    let getDateNow = () => new Date().getTime();
+    let deleteIntervalHandler = () => {
+        clearInterval(handler);
+        handler = 0;
+    };
+    let setTime = (newTime) => {
+        time = newTime;
+        if(container){
+            container.innerText = newTime;
+        }
+        options.callback(newTime);
+    };
+    let setUpdateInterval = (newInterval) => updateInterval = newInterval;
+    this.start = (newInterval) => {
+        if(newInterval){
+            setUpdateInterval(newInterval);
+        }
+        setTime(time);
+        let timeToCount = toTimestamp(time);
+        let startTime = getDateNow();
+        deleteIntervalHandler();
+        handler = setInterval(()=>{
+            let updateDate = getDateNow();
+            let timeDiff = updateDate - startTime;
+            let realTime = timeToCount + timeDiff;
+            let timeValue = formatTime(realTime);
+            setTime(timeValue);
+        }, updateInterval);
+    };
+    this.stop = () => deleteIntervalHandler();
+    this.reset = (newInterval) => {
+        deleteIntervalHandler();
+        setTime(0);
+    };
+    this.getTime = () => Number(time);
+    this.isRunning = () => Boolean(handler);
 }
+
