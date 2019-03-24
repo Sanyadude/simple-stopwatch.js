f838bef625dd114016802555d1e923eb61e76482
Sanyadude <sanyadude@i.ua>
2019-03-24 13:35:22 +0200

    add events callbacks

--- README.md
+++ README.md
@@ -14,30 +14,28 @@ var stopwatchOptions = {
 var stopwatch = new SimpleStopwatch(stopwatchOptions);
 stopwatch.start();
 </pre>
-</br>
 Methods to manipulate stopwatch -
-</br>
 Start counting time
 <pre>
 stopwatch.start()
 </pre>
-</br>
 Stop counting time
 <pre>
 stopwatch.stop()
 </pre>
-</br>
 Reset stopwatch
 <pre>
 stopwatch.reset()
 </pre>
-</br>
 Get current stopwatch time
 <pre>
 stopwatch.getTime()
 </pre>
-</br>
 Get state of stopwatch running
 <pre>
 stopwatch.isRunning()
+</pre>
+Add callback to timer methods, available names ['start','stop','update','reset'], update callback will have time from stopwatch as parameter
+<pre>
+stopwatch.on(name, callback)
 </pre>
--- simple-stopwatch.js
+++ simple-stopwatch.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var SimpleStopwatch = function(customOptions) {
+var SimpleStopwatch = function (customOptions) {
     let options = customOptions ? customOptions : {};
     let jsSecond = 1000;
     let time = options.startTime ? options.startTime : 0;
@@ -8,8 +8,14 @@ var SimpleStopwatch = function(customOptions) {
     let precision = options.precision ? options.precision : 0;
     let container = document.getElementById(options.containerId);
     let handler;
-    let formatTime = (milliseconds) =>(milliseconds/jsSecond).toFixed(precision);
-    let toTimestamp = (time) => Number(time)*jsSecond;
+    let callbacks = {
+        start: [],
+        stop: [],
+        update: [],
+        reset: [],
+    };
+    let formatTime = (milliseconds) => (milliseconds / jsSecond).toFixed(precision);
+    let toTimestamp = (time) => Number(time) * jsSecond;
     let getDateNow = () => new Date().getTime();
     let deleteIntervalHandler = () => {
         clearInterval(handler);
@@ -17,35 +23,48 @@ var SimpleStopwatch = function(customOptions) {
     };
     let setTime = (newTime) => {
         time = newTime;
-        if(container){
+        if (container) {
             container.innerText = newTime;
         }
-        if(options.callback) {
-            options.callback(newTime);
-        }
+        executeCallbacks('update', newTime);
     };
     let setUpdateInterval = (newInterval) => updateInterval = newInterval;
+    let executeCallbacks = (eventName, eventParam) => {
+        callbacks[eventName].forEach(function (event) {
+            event(eventParam);
+        })
+    };
     this.start = (newInterval) => {
-        if(newInterval){
+        if (newInterval) {
             setUpdateInterval(newInterval);
         }
         setTime(time);
         let timeToCount = toTimestamp(time);
         let startTime = getDateNow();
         deleteIntervalHandler();
-        handler = setInterval(()=>{
+        handler = setInterval(() => {
             let updateDate = getDateNow();
             let timeDiff = updateDate - startTime;
             let realTime = timeToCount + timeDiff;
             let timeValue = formatTime(realTime);
             setTime(timeValue);
         }, updateInterval);
+        executeCallbacks('start', );
     };
-    this.stop = () => deleteIntervalHandler();
+    this.stop = () => {
+        deleteIntervalHandler();
+        executeCallbacks('stop');
+    };
     this.reset = (newInterval) => {
         deleteIntervalHandler();
         setTime(0);
+        executeCallbacks('reset');
     };
     this.getTime = () => Number(time);
     this.isRunning = () => Boolean(handler);
+    this.on = (eventName, callback) => {
+        if (callbacks.hasOwnProperty(eventName)) {
+            callbacks[eventName].push(callback);
+        }
+    };
 }
